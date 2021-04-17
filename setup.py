3007018a90071ea283d6af28ff6ec7a36310daf2
minusmagis <minu_10@hotmail.com>
2021-04-17 11:07:56 +0200

    It works now

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 
 ########################################################################################################################
 
-plugin_identifier = "psucontrolautoconnect"
+plugin_identifier = "psucontrol"
 plugin_package = "octoprint_%s" % plugin_identifier
-plugin_name = "OctoPrint-PSUControl-Autoconnect"
-plugin_version = "0.0.1"
-plugin_description = "Control ATX/AUX power supply and connect to printer automatically."
-plugin_author = "Minusmagis"
-plugin_author_email = "minusmagis@hotmail.com"
-plugin_url = "https://github.com/minusmagis/OctoPrint-PSUControl-With-Autoconnect"
+plugin_name = "OctoPrint-PSUControl"
+plugin_version = "1.0.1"
+plugin_description = "Control ATX/AUX power supply."
+plugin_author = "Shawn Bruce"
+plugin_author_email = "kantlivelong@gmail.com"
+plugin_url = "https://github.com/kantlivelong/OctoPrint-PSUControl"
 plugin_license = "AGPLv3"
 plugin_additional_data = []
 
